5b24fd953a15fffbc0b2d68bed86ae749abda569
amonetlol <65382640+amonetlol@users.noreply.github.com>
2024-10-19 13:41:18 -0300

    Create temas.sh

---
+++ temas.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+theme_1(){
+echo "Qogir"
+    yay -S --needed --noconfirm qogir-gtk-theme qogir-icon-theme gtk-engine-murrine gtk-engines gnome-themes-extra
+}
+
+theme_2(){
+    echo "Catppuccin-Mocha"
+    yay -S --needed --noconfirm catppuccin-gtk-theme-mocha mcmojave-circle-icon-theme-git catppuccin-cursors-mocha gtk-engine-murrine gtk-engines gnome-themes-extra    
+}
+
+theme_1
+theme_2
